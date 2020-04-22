821d2e45eff9fc7435e4c551227e244f26ac0278
huyha <huy.ha@vmodev.com>
2020-04-22 16:02:07 +0700

    Change commit message format

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var inquirer = require('inquirer')
+var inquirer = require('inquirer');
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
 module.exports = {
   prompter: prompter,
-  formatCommit: formatCommit
+  formatCommit: formatCommit,
 };
 
 // When a user runs `git cz`, prompter will
@@ -20,7 +20,6 @@ module.exports = {
 // By default, we'll de-indent your commit
 // template and will keep empty lines.
 function prompter(cz, commit) {
-
   // Let's ask some questions of the user
   // so that we can populate our commit
   // template.
@@ -28,70 +27,107 @@ function prompter(cz, commit) {
   // See inquirer.js docs for specifics.
   // You can also opt to use another input
   // collection library if you prefer.
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'message',
-      message: 'GitHub commit message (required):\n',
-      validate: function(input) {
-        if (!input) {
-          return 'empty commit message';
-        } else {
-          return true;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'issues',
-      message: 'Jira Issue ID(s) (required):\n',
-      validate: function(input) {
-        if (!input) {
-          return 'Must specify issue IDs, otherwise, just use a normal commit message';
-        } else {
-          return true;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'workflow',
-      message: 'Workflow command (testing, closed, etc.) (optional):\n',
-      validate: function(input) {
-        if (input && input.indexOf(' ') !== -1) {
-          return 'Workflows cannot have spaces in smart commits. If your workflow name has a space, use a dash (-)';
-        } else {
-          return true;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'time',
-      message: 'Time spent (i.e. 3h 15m) (optional):\n'
-    },
-    {
-      type: 'input',
-      name: 'comment',
-      message: 'Jira comment (optional):\n'
-    },
-  ]).then((answers) => {
-    formatCommit(commit, answers);
-  });
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'type',
+        message:
+          "Select the type of change that you're committing (required):\n",
+        choices: [
+          { value: 'Feat', name: 'Feat:     A new feature' },
+          { value: 'Fix', name: 'Fix:      A bug fix' },
+          { value: 'Docs', name: 'Docs:     Documentation only changes' },
+          {
+            value: 'Style',
+            name:
+              'Style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+          },
+          {
+            value: 'Refactor',
+            name:
+              'Refactor: A code change that neither fixes a bug nor adds a feature',
+          },
+          {
+            value: 'Perf',
+            name: 'Perf:     A code change that improves performance',
+          },
+          { value: 'Test', name: 'Test:     Adding missing tests' },
+          {
+            value: 'Chore',
+            name:
+              'Chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+          },
+        ],
+      },
+      {
+        type: 'input',
+        name: 'issues',
+        message: 'Jira Issue ID(s):\n',
+      },
+      {
+        type: 'input',
+        name: 'message',
+        message: 'Commit message (required):\n',
+        validate: function (input) {
+          if (!input) {
+            return 'empty commit message';
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: 'input',
+        name: 'workflow',
+        message:
+          'Workflow command (eg: to-do, in-progress, in-review, in-testing, done, etc.) (optional):\n',
+        validate: function (input) {
+          if (input && input.indexOf(' ') !== -1) {
+            return 'Workflows cannot have spaces in smart commits. If your workflow name has a space, use a dash (-)';
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: 'input',
+        name: 'time',
+        message: 'Time spent (i.e. 3h 15m) (optional):\n',
+      },
+      {
+        type: 'input',
+        name: 'comment',
+        message: 'Jira comment (optional):\n',
+      },
+      {
+        type: 'input',
+        name: 'info',
+        message:
+          'More info for detail (eg: link to JIRA task or any notes) (optional):\n',
+      },
+    ])
+    .then((answers) => {
+      formatCommit(commit, answers);
+    });
 }
 
 function formatCommit(commit, answers) {
-  commit(filter([
-    answers.message,
-    answers.issues,
-    answers.workflow ? '#' + answers.workflow : undefined,
-    answers.time ? '#time ' + answers.time : undefined,
-    answers.comment ? '#comment ' + answers.comment : undefined,
-  ]).join(' '));
+  commit(
+    filter([
+      answers.type + ': ',
+      answers.issues ? '[' + answers.issues + ']' : undefined,
+      answers.message,
+      answers.workflow ? '#' + answers.workflow : undefined,
+      answers.time ? '#time ' + answers.time : undefined,
+      answers.comment ? '#comment ' + answers.comment : undefined,
+      answers.info ? '\n\n' + answers.info : undefined,
+    ]).join(' ')
+  );
 }
 
 function filter(array) {
-  return array.filter(function(item) {
+  return array.filter(function (item) {
     return !!item;
   });
 }
--- index.test.js
+++ index.test.js
@@ -7,16 +7,16 @@ chai.use(sinonChai);
 const czJiraSmartCommit = require('./index.js');
 
 describe('prompt for inputs', () => {
-  it('should be a function',  () => {
+  it('should be a function', () => {
     expect(czJiraSmartCommit.prompter).to.be.a('function');
   });
 });
 
 describe('format commits', () => {
-
-  const message = 'sample commit message';
+  const type = 'feat';
   const issues = 'CZ-234 CZ-235';
-  const workflow = 'closed';
+  const message = 'sample commit message';
+  const workflow = 'done';
   const time = '3y 2w 7d 8h 30m';
   const comment = 'This took waaaaay too long';
 
@@ -24,23 +24,43 @@ describe('format commits', () => {
     expect(czJiraSmartCommit.formatCommit).to.be.a('function');
   });
   it('should perform a full commit', () => {
-    czJiraSmartCommit.formatCommit((result) => {
-      expect(result).to.equal('sample commit message CZ-234 CZ-235 #closed #time 3y 2w 7d 8h 30m #comment This took waaaaay too long')
-    }, {message, issues, workflow, time, comment});
+    czJiraSmartCommit.formatCommit(
+      (result) => {
+        expect(result).to.equal(
+          'feat:  [CZ-234 CZ-235] sample commit message #done #time 3y 2w 7d 8h 30m #comment This took waaaaay too long'
+        );
+      },
+      { type, issues, message, workflow, time, comment }
+    );
   });
   it('should commit without a workflow', () => {
-    czJiraSmartCommit.formatCommit((result) => {
-      expect(result).to.equal('sample commit message CZ-234 CZ-235 #time 3y 2w 7d 8h 30m #comment This took waaaaay too long')
-    }, {message, issues, time, comment});
+    czJiraSmartCommit.formatCommit(
+      (result) => {
+        expect(result).to.equal(
+          'feat:  [CZ-234 CZ-235] sample commit message #time 3y 2w 7d 8h 30m #comment This took waaaaay too long'
+        );
+      },
+      { type, issues, message, time, comment }
+    );
   });
   it('should commit without a time', () => {
-    czJiraSmartCommit.formatCommit((result) => {
-      expect(result).to.equal('sample commit message CZ-234 CZ-235 #closed #comment This took waaaaay too long')
-    }, {message, issues, workflow, comment});
+    czJiraSmartCommit.formatCommit(
+      (result) => {
+        expect(result).to.equal(
+          'feat:  [CZ-234 CZ-235] sample commit message #done #comment This took waaaaay too long'
+        );
+      },
+      { type, issues, message, workflow, comment }
+    );
   });
   it('should commit without a comment', () => {
-    czJiraSmartCommit.formatCommit((result) => {
-      expect(result).to.equal('sample commit message CZ-234 CZ-235 #closed #time 3y 2w 7d 8h 30m')
-    }, {message, issues, workflow, time});
+    czJiraSmartCommit.formatCommit(
+      (result) => {
+        expect(result).to.equal(
+          'feat:  [CZ-234 CZ-235] sample commit message #done #time 3y 2w 7d 8h 30m'
+        );
+      },
+      { type, issues, message, workflow, time }
+    );
   });
 });
